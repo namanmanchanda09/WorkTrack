519a96332e958b86aa4e22986e105b1678d853fd
Naman Manchanda <namanmanchanda@Namans-MacBook-Air.local>
2020-11-07 15:02:38 +0530

    added the Views

---
+++ WorkTrack/Controllers/WelcomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  WorkTrack
+//
+//  Created by Naman Manchanda on 07/11/20.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+
